f20bb20e276420d1540bd8cc547ad5a325d053ee
Brian Grant and Matthew Reyes <brian@>
2016-05-31 13:44:52 -0700

    create initial directory structure, index.html, and flashcard elements using jQuery and bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+  $(".click-java").click(function() {
+    $(".hidden-text.javascript").toggle();
+    $(".shown-text.javascript").toggle();
+  });
+
+  $(".click-operators").click(function() {
+    $(".hidden-text.operators").toggle();
+    $(".shown-text.operators").toggle();
+  });
+
+  $(".click-variables").click(function() {
+    $(".hidden-text.variables").toggle();
+    $(".shown-text.variables").toggle();
+  });
+
+  $(".click-variableName").click(function() {
+    $(".hidden-text.variableName").toggle();
+    $(".shown-text.variableName").toggle();
+  });
+
+  $(".click-functions").click(function() {
+    $(".hidden-text.functions").toggle();
+    $(".shown-text.functions").toggle();
+  });
+
+  $(".click-methods").click(function() {
+    $(".hidden-text.methods").toggle();
+    $(".shown-text.methods").toggle();
+  });
+
+  $(".click-arguments").click(function() {
+    $(".hidden-text.arguments").toggle();
+    $(".shown-text.arguments").toggle();
+  });
+
+  $(".click-parameters").click(function() {
+    $(".hidden-text.parameters").toggle();
+    $(".shown-text.parameters").toggle();
+  });
+
+  $(".click-return").click(function() {
+    $(".hidden-text.return").toggle();
+    $(".shown-text.return").toggle();
+  });
+
+  $(".click-chain-methods").click(function() {
+    $(".hidden-text.chain-methods").toggle();
+    $(".shown-text.chain-methods").toggle();
+  });
+
+  $(".click-strings").click(function() {
+    $(".hidden-text.strings").toggle();
+    $(".shown-text.strings").toggle();
+  });
+
+  $(".click-booleans").click(function() {
+    $(".hidden-text.booleans").toggle();
+    $(".shown-text.booleans").toggle();
+  });
+
+  $(".click-undefined").click(function() {
+    $(".hidden-text.undefined").toggle();
+    $(".shown-text.undefined").toggle();
+  });
+
+  $(".click-NaN").click(function() {
+    $(".hidden-text.NaN").toggle();
+    $(".shown-text.NaN").toggle();
+  });
+
+  $(".click-Escape").click(function() {
+    $(".hidden-text.Escape").toggle();
+    $(".shown-text.Escape").toggle();
+  });
+
+  $(".click-Alert").click(function() {
+    $(".hidden-text.Alert").toggle();
+    $(".shown-text.Alert").toggle();
+  });
+
+  $(".click-comments").click(function() {
+    $(".hidden-text.comments").toggle();
+    $(".shown-text.comments").toggle();
+  });
+
+  $(".click-jQuery").click(function() {
+    $(".hidden-text.jQuery").toggle();
+    $(".shown-text.jQuery").toggle();
+  });
+
+  $(".click-attributes").click(function() {
+    $(".hidden-text.attributes").toggle();
+    $(".shown-text.attributes").toggle();
+  });
+
+
+});
